7139830ec25fa8037e5a10956e7e5ac68ab0dc1c
Luca Ambrosi <luca@luca.it>
2023-08-27 15:40:22 +0200

    test puppeteer

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const path = require("path")
 const nextConfig = {
     sassOptions: {
         includePaths: [path.join(__dirname, "styles")]
+    },
+
+    reactStrictMode: true,
+    output: "standalone",
+    experimental: {
+        outputFileTracingRoot: path.join(__dirname, "../../"),
+        transpilePackages: ["ui"]
     }
 }
 
