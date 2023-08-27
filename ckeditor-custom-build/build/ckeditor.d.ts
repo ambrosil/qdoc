de84b64dfd016429d70245dec03f0a3f799a06d3
Luca Ambrosi <luca@luca.it>
2023-08-27 13:02:22 +0200

    fix build next.js con ckeditor

--- ckeditor-custom-build/build/ckeditor.d.ts
+++ ckeditor-custom-build/build/ckeditor.d.ts
@@ -2,30 +2,29 @@
  * @license Copyright (c) 2014-2023, CKSource Holding sp. z o.o. All rights reserved.
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
-import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
-import { Alignment } from '@ckeditor/ckeditor5-alignment';
-import { Autoformat } from '@ckeditor/ckeditor5-autoformat';
-import { Bold, Italic, Strikethrough, Underline } from '@ckeditor/ckeditor5-basic-styles';
-import { CloudServices } from '@ckeditor/ckeditor5-cloud-services';
-import { Essentials } from '@ckeditor/ckeditor5-essentials';
-import { FontBackgroundColor, FontColor, FontFamily, FontSize } from '@ckeditor/ckeditor5-font';
-import { Heading } from '@ckeditor/ckeditor5-heading';
-import { HorizontalLine } from '@ckeditor/ckeditor5-horizontal-line';
-import { HtmlEmbed } from '@ckeditor/ckeditor5-html-embed';
-import { GeneralHtmlSupport } from '@ckeditor/ckeditor5-html-support';
-import { Image, ImageInsert, ImageResize, ImageStyle, ImageToolbar, ImageUpload } from '@ckeditor/ckeditor5-image';
-import { Indent, IndentBlock } from '@ckeditor/ckeditor5-indent';
-import { List } from '@ckeditor/ckeditor5-list';
-import { PageBreak } from '@ckeditor/ckeditor5-page-break';
-import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
-import { PasteFromOffice } from '@ckeditor/ckeditor5-paste-from-office';
-import { RemoveFormat } from '@ckeditor/ckeditor5-remove-format';
-import { StandardEditingMode } from '@ckeditor/ckeditor5-restricted-editing';
-import { SpecialCharacters, SpecialCharactersCurrency, SpecialCharactersEssentials, SpecialCharactersText } from '@ckeditor/ckeditor5-special-characters';
-import { Table, TableCaption, TableCellProperties, TableColumnResize, TableProperties, TableToolbar } from '@ckeditor/ckeditor5-table';
-import { TextTransformation } from '@ckeditor/ckeditor5-typing';
+import { ClassicEditor } from "@ckeditor/ckeditor5-editor-classic";
+import { Alignment } from "@ckeditor/ckeditor5-alignment";
+import { Autoformat } from "@ckeditor/ckeditor5-autoformat";
+import { Bold, Italic, Strikethrough, Underline } from "@ckeditor/ckeditor5-basic-styles";
+import { CloudServices } from "@ckeditor/ckeditor5-cloud-services";
+import { Essentials } from "@ckeditor/ckeditor5-essentials";
+import { FontBackgroundColor, FontColor, FontFamily, FontSize } from "@ckeditor/ckeditor5-font";
+import { Heading } from "@ckeditor/ckeditor5-heading";
+import { HorizontalLine } from "@ckeditor/ckeditor5-horizontal-line";
+import { HtmlEmbed } from "@ckeditor/ckeditor5-html-embed";
+import { GeneralHtmlSupport } from "@ckeditor/ckeditor5-html-support";
+import { Image, ImageInsert, ImageResize, ImageStyle, ImageToolbar, ImageUpload } from "@ckeditor/ckeditor5-image";
+import { Indent, IndentBlock } from "@ckeditor/ckeditor5-indent";
+import { List } from "@ckeditor/ckeditor5-list";
+import { PageBreak } from "@ckeditor/ckeditor5-page-break";
+import { Paragraph } from "@ckeditor/ckeditor5-paragraph";
+import { RemoveFormat } from "@ckeditor/ckeditor5-remove-format";
+import { StandardEditingMode } from "@ckeditor/ckeditor5-restricted-editing";
+import { SpecialCharacters, SpecialCharactersCurrency, SpecialCharactersEssentials, SpecialCharactersText } from "@ckeditor/ckeditor5-special-characters";
+import { Table, TableCaption, TableCellProperties, TableColumnResize, TableProperties, TableToolbar } from "@ckeditor/ckeditor5-table";
+import { TextTransformation } from "@ckeditor/ckeditor5-typing";
 declare class Editor extends ClassicEditor {
-    static builtinPlugins: (typeof Alignment | typeof TextTransformation | typeof Autoformat | typeof Bold | typeof Italic | typeof Strikethrough | typeof Underline | typeof CloudServices | typeof Essentials | typeof FontBackgroundColor | typeof FontColor | typeof FontFamily | typeof FontSize | typeof Paragraph | typeof Heading | typeof HorizontalLine | typeof HtmlEmbed | typeof GeneralHtmlSupport | typeof Image | typeof ImageInsert | typeof ImageResize | typeof ImageStyle | typeof ImageToolbar | typeof ImageUpload | typeof Indent | typeof IndentBlock | typeof List | typeof PageBreak | typeof PasteFromOffice | typeof RemoveFormat | typeof StandardEditingMode | typeof SpecialCharacters | typeof SpecialCharactersText | typeof SpecialCharactersEssentials | typeof SpecialCharactersCurrency | typeof Table | typeof TableCaption | typeof TableCellProperties | typeof TableColumnResize | typeof TableProperties | typeof TableToolbar)[];
+    static builtinPlugins: (typeof Alignment | typeof Autoformat | typeof Bold | typeof CloudServices | typeof Essentials | typeof FontBackgroundColor | typeof FontColor | typeof FontFamily | typeof FontSize | typeof GeneralHtmlSupport | typeof Heading | typeof HorizontalLine | typeof HtmlEmbed | typeof Image | typeof ImageInsert | typeof ImageResize | typeof ImageStyle | typeof ImageToolbar | typeof ImageUpload | typeof Indent | typeof IndentBlock | typeof Italic | typeof List | typeof PageBreak | typeof Paragraph | typeof RemoveFormat | typeof SpecialCharacters | typeof SpecialCharactersCurrency | typeof SpecialCharactersEssentials | typeof SpecialCharactersText | typeof StandardEditingMode | typeof Strikethrough | typeof Table | typeof TableCaption | typeof TableCellProperties | typeof TableColumnResize | typeof TableProperties | typeof TableToolbar | typeof TextTransformation | typeof Underline)[];
     static defaultConfig: {
         toolbar: {
             items: string[];
