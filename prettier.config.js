3d62a4e08a3ef7331686e59bb1716b582e754303
Luca Ambrosi <luca@luca.it>
2023-08-27 12:05:48 +0200

    linting

--- prettier.config.js
+++ prettier.config.js
@@ -2,6 +2,8 @@ module.exports = {
     semi: false,
     trailingComma: "none",
     tabWidth: 4,
+    singleQuote: true,
+    useTabs: true,
     jsxSingleQuote: true,
     printWidth: 120,
     plugins: [import("prettier-plugin-tailwindcss")]
